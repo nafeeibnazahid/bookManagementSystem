e0f2f103f813778a226988a8cb81b8688aa2d8a0
nafeeibnazahid <md.zahid@bkash.com>
2024-03-15 11:18:27 +0600

    Added BookManagement Application under services

---
+++ src/main/java/com/bkash/bookmanagement/services/BookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bkash.bookmanagement.services;
+
+import com.bkash.bookmanagement.Main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+//        # sdfdsaf
+    }
+}
