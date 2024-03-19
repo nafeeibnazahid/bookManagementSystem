58f08741d669b43c47c896841422e1e264f30a1c
nafeeibnazahid <md.zahid@bkash.com>
2024-03-19 22:50:26 +0600

    added a globalexception handler

--- src/main/java/com/bkash/bookmanagement/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/bkash/bookmanagement/exceptionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.bkash.bookmanagement.exceptionHandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Object> handleException(Exception exception) {
+        log.error(exception.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+                .body("Error occurred");
     }
 }
